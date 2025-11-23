71494b7acbcade7cdbca1cdd0d4559b924fcc6b8
r11b22 <joost.lehaen@gmail.com>
2025-11-23 21:11:12 +0100

    Created v0.0.1

---
+++ SystemReloader/SystemReloader.cs
@@ -0,0 +1,34 @@
+﻿using Brutal.ImGuiApi;
+using StarMap.API;
+using KSA;
+using System.Diagnostics;
+
+namespace SystemReloader
+{
+    [StarMapMod]
+    public class SystemReloader
+    {
+        [StarMapAfterGui]
+        public void guiInit(double dt)
+        {
+            ImGui.Begin("System Reloader");
+            if(ImGui.Button("Reload System"))
+            {
+                Console.WriteLine("Reloading game");
+
+                string exePath = Process.GetCurrentProcess().MainModule.FileName;
+
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = exePath,
+                    WorkingDirectory = Path.GetDirectoryName(exePath), // Put the working directory back to StarMap
+                    UseShellExecute = true
+                };
+
+                Process.Start(startInfo);
+                Program.Instance.RequestExit(0);
+            }
+            ImGui.End();
+        }
+    }
+}
